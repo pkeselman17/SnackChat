cf68e95a83dca06b6c8bdb624fb1bc7f3f66ba67
tyler_cunnington <cunnington.3@osu.edu>
2015-04-22 13:56:37 -0400

    linked login to groups

--- app/src/main/java/com/snackchat/snackchat/Login.java
+++ app/src/main/java/com/snackchat/snackchat/Login.java
@@ -1,13 +1,13 @@
 package com.snackchat.snackchat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
+import android.view.View;
 import android.widget.Button;
-
-import com.parse.ParseObject;
+import android.widget.EditText;
 
 
 public class Login extends Activity {
@@ -39,10 +39,12 @@ public class Login extends Activity {
         login = (Button) findViewById(R.id.buttonLogin);
     }
 
-    public void loginAttempt() {
+    public void loginAttempt(View view) {
         // if login attempt successful, send them to my groups screen
         // else, toast statement saying unsuccessful login
 
+        Intent in = new Intent(getBaseContext(), GroupsPage.class);
+        startActivity(in);
     }
 
     @Override
